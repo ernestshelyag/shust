2e330d05260501860724425ec0b902f81a294056
Eric <ernest.shelyag@gmail.com>
2018-06-06 11:58:09 +0300

    test modal

--- src/components/modals/modals.js
+++ src/components/modals/modals.js
@@ -1,10 +1,6 @@
 function modals () {
-  // forms
-  $('.hero-slider-text__btn,' +
-    '.header-top__enter,' +
-    '.modal__reg,' +
-    '.price__top-btn,' +
-    '.price-item__btn').magnificPopup({
+  // forms - focus mail
+  $('.header-top__enter').magnificPopup({
     type: 'inline',
     preloader: false,
     removalDelay: 300,
@@ -20,7 +16,27 @@ function modals () {
       }
     }
   });
-
+  // forms - focus name
+  $('.hero-slider-text__btn,' +
+    '.test-block__btn,' +
+    '.modal__reg,' +
+    '.price__top-btn,' +
+    '.price-item__btn').magnificPopup({
+    type: 'inline',
+    preloader: false,
+    removalDelay: 300,
+    mainClass: 'my-mfp-animate',
+    focus: '.modal__input_name',
+    callbacks: {
+      beforeOpen: function () {
+        if ($(window).width() < 700) {
+          this.st.focus = false;
+        } else {
+          this.st.focus = '.modal__input_name';
+        }
+      }
+    }
+  });
   // video
   $('.btn-play, .video li a, .review__slider_video a').magnificPopup({
     mainClass: 'my-mfp-video',
