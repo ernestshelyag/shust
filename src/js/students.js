4ffdfbb7848499b01954dcb462742d43c6a42fde
Eric <ernest.shelyag@gmail.com>
2018-06-06 14:20:31 +0300

    students

--- src/js/students.js
+++ src/js/students.js
@@ -1,7 +1,20 @@
 function students () {
+  // $('.students__pdf-title').click(function () {
+  //   $(this).toggleClass('active');
+  //   $(this).siblings('.students__items-hiding').slideToggle();
+  // });
   $('.students__pdf-title').click(function () {
-    $(this).toggleClass('active');
-    $(this).siblings('.students__items-hiding').slideToggle();
+    // -
+    $('.students__pdf-title').not($(this)).removeClass('active');
+    $('.students__items-hiding').slideUp(600);
+    // -
+    if ($(this).hasClass('active')) {
+      $('.students__pdf-title').removeClass('active');
+      $('.students__items-hiding').slideUp(600);
+    } else {
+      $(this).addClass('active');
+      $(this).next('.students__items-hiding').slideDown(600);
+    }
   });
   // -
   $('[data-tab]').on('click', function () {
